7a1172dd61f971400e97fec0ca7c28a3b82fea73
Robert Evans <robert@skutchi.com>
2025-07-29 13:44:43 -0400

    get_shipping_services

--- scloud_api/api/settings/settings.py
+++ scloud_api/api/settings/settings.py
@@ -3,3 +3,6 @@ from scloud_api.base import Client, ApiResponse
 class Settings(Client):
     def get_shipping_carriers(self, site_code=0) -> ApiResponse:
         return self._request('/api/Settings/ShippingCarriers', "GET", {'siteCode': site_code})
+
+    def get_shipping_services(self, carrier, site_code=0) -> ApiResponse:
+        return self._request('/api/Settings/ShippingServices', 'GET', {'carrier': carrier, 'siteCode': site_code})

7c91fb8d15c789567b16bcd7d89cbfbda17e1f59
Robert Evans <robert@skutchi.com>
2025-07-29 14:14:13 -0400

    Add better error handling in ApiResponse

--- scloud_api/base/ApiResponse.py
+++ scloud_api/base/ApiResponse.py
@@ -1,5 +1,9 @@
 class ApiResponse:
-    def __init__(self, status_code, response):
+    def __init__(self, status_code, response, error):
         self.status_code = status_code
         self.payload = response['Items'] if 'Items' in response else response
         self.total_results = response['TotalResults'] if 'TotalResults' in response else None
+        if status_code != 200 and response:
+            self.error = response
+        else:
+            self.error = error
--- scloud_api/base/client.py
+++ scloud_api/base/client.py
@@ -2,30 +2,45 @@ from requests import request
 import json
 from scloud_api.auth import AuthTokenClient
 from .ApiResponse import ApiResponse
+from ..auth.exceptions import AuthException
 
 
 class Client:
     def __init__(self, cred=None):
-        auth = AuthTokenClient(cred)
-        self._auth = auth.get_auth()
+        self.auth = AuthTokenClient(cred)
 
     def _request(self, path: str, method: str, data: dict = None) -> ApiResponse:
+        try:
+            self._auth = self.auth.get_auth()
+        except AuthException as e:
+            return ApiResponse(401,{}, e.message)
+
         endpoint = f"{self._auth.url}{path}"
-        res = request(
-            method=method,
-            url=endpoint,
-            headers=self.headers,
-            data=(
-                json.dumps(data)
-                if data and method in ("POST", "PUT", "PATCH")
-                else None
-            ),
-            params=(
-                data
-                if data and method in ("GET")
-                else None
-            ))
-        return ApiResponse(res.status_code, res.json())
+        error = ""
+        status_code = 0
+        result = {}
+
+        try:
+            res = request(
+                method=method,
+                url=endpoint,
+                headers=self.headers,
+                data=(
+                    json.dumps(data)
+                    if data and method in ("POST", "PUT", "PATCH")
+                    else None
+                ),
+                params=(
+                    data
+                    if data and method in ("GET")
+                    else None
+                ))
+            status_code = res.status_code
+            result = res.json()
+        except AuthException as e:
+            error = e.message
+            status_code = 401
+        return ApiResponse(status_code, result, error)
 
     @property
     def headers(self):

35de09b3b772c07cdc3c383913944a1c77f03bdf
Robert Evans <robert@skutchi.com>
2025-07-29 13:33:23 -0400

    Change function name and add pydoc

--- scloud_api/api/queued_jobs/queued_jobs.py
+++ scloud_api/api/queued_jobs/queued_jobs.py
@@ -1,5 +1,11 @@
 from scloud_api.base import Client, ApiResponse
 
 class QueuedJobs(Client):
-    def get_job_status(self, job_id) -> ApiResponse:
+    def get_queued_job(self, job_id) -> ApiResponse:
+        """
+        get_queued_job(self, job_id) -> ApiResponse
+
+        :param job_id: int the job id to
+        :return: ApiResponse
+        """
         return self._request(f"/api/QueuedJobs/{job_id}", "GET")
